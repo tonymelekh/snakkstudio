9cd7ab3815c5afbdc83e2c40fbace53906db5de8
Danik <danik.cyrkunov@gmail.com>
2020-10-30 17:27:58 +0300

    Update project_page.js

--- project_page.js
+++ project_page.js
@@ -65,45 +65,48 @@ var startSelect = document.querySelectorAll('[data-video-size="16-9"]')[0].child
   document.querySelectorAll(".js-button-video-size").forEach(function(item) {
 	$(item).on("click touchstart", function(e) {
       var videoSize = e.target.dataset.videoSizeButton
-      console.log("aaaaaaaaa: ", videoSize)
       
-      if (document.querySelectorAll('[data-video-size-button="9-16"]') && document.querySelectorAll('[data-video-size-button="9-16"]').length) {
-      	document.querySelectorAll('[data-video-size-button="9-16"]').forEach(item => {
-            item.style.fontWeight = 400;
-            item.style.color = "#979797";
-          })
-      }
-      if (document.querySelectorAll('[data-video-size-button="1-1"]') && document.querySelectorAll('[data-video-size-button="1-1"]').length) {
-      	document.querySelectorAll('[data-video-size-button="1-1"]').forEach(item => {
-            item.style.fontWeight = 400;
-            item.style.color = "#979797";
-          })
-      }
-      if (document.querySelectorAll('[data-video-size-button="4-5"]') && document.querySelectorAll('[data-video-size-button="4-5"]').length) {
-      	document.querySelectorAll('[data-video-size-button="4-5"]').forEach(item => {
-            item.style.fontWeight = 400;
-            item.style.color = "#979797";
-          })
-      }
-      if (document.querySelectorAll('[data-video-size-button="16-9"]') && document.querySelectorAll('[data-video-size-button="16-9"]').length) {
-      	document.querySelectorAll('[data-video-size-button="16-9"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
-      }
-      
-      document.querySelectorAll('[data-video-size-button="' + videoSize + '"]').forEach(item => {item.style.fontWeight = 900; item.style.color = "white";})
-      $('[data-video-size="' + selectedVideo + '"]').animate({
-	opacity: 0
-      }, 400, function() {
-	      if (selectedVideo !== videoSize) {
-		      $(document.querySelectorAll('[data-video-size="' + videoSize + '"]')).css('display', 'block')
-		      $('[data-video-size="' + selectedVideo + '"]').css('display', "none")
-		      $(document.querySelectorAll('[data-video-size="' + videoSize + '"]')).css('opacity', 0)
-		      $(document.querySelectorAll('[data-video-size="' + videoSize + '"]')).animate({
-			opacity: 1
-		      }, 400)
-		      selectedVideo = videoSize
+      if (selectedVideo !== videoSize) {
+	      if (document.querySelectorAll('[data-video-size-button="9-16"]') && document.querySelectorAll('[data-video-size-button="9-16"]').length) {
+		document.querySelectorAll('[data-video-size-button="9-16"]').forEach(item => {
+		    item.style.fontWeight = 400;
+		    item.style.color = "#979797";
+		  })
+	      }
+	      if (document.querySelectorAll('[data-video-size-button="1-1"]') && document.querySelectorAll('[data-video-size-button="1-1"]').length) {
+		document.querySelectorAll('[data-video-size-button="1-1"]').forEach(item => {
+		    item.style.fontWeight = 400;
+		    item.style.color = "#979797";
+		  })
+	      }
+	      if (document.querySelectorAll('[data-video-size-button="4-5"]') && document.querySelectorAll('[data-video-size-button="4-5"]').length) {
+		document.querySelectorAll('[data-video-size-button="4-5"]').forEach(item => {
+		    item.style.fontWeight = 400;
+		    item.style.color = "#979797";
+		  })
+	      }
+	      if (document.querySelectorAll('[data-video-size-button="16-9"]') && document.querySelectorAll('[data-video-size-button="16-9"]').length) {
+		document.querySelectorAll('[data-video-size-button="16-9"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
 	      }
 
-      })
+	      document.querySelectorAll('[data-video-size-button="' + videoSize + '"]').forEach(item => {item.style.fontWeight = 900; item.style.color = "white";})
+	      $('[data-video-size="' + selectedVideo + '"]').animate({
+		opacity: 0
+	      }, 400, function() {
+		      if (selectedVideo !== videoSize) {
+			      $(document.querySelectorAll('[data-video-size="' + videoSize + '"]')).css('display', 'block')
+			      $('[data-video-size="' + selectedVideo + '"]').css('display', "none")
+			      $(document.querySelectorAll('[data-video-size="' + videoSize + '"]')).css('opacity', 0)
+			      $(document.querySelectorAll('[data-video-size="' + videoSize + '"]')).animate({
+				opacity: 1
+			      }, 400)
+			      selectedVideo = videoSize
+		      }
+
+	      })
+      }
+      
+
     })
   })
 
@@ -123,38 +126,40 @@ var startSelect = document.querySelectorAll('[data-video-size-button="1"]')
     document.querySelectorAll('[data-video-size="1"]').forEach(item => {item.style.display = "block"; selectedVideo2 = "1"})
   } 
   document.querySelectorAll(".js-button-video-size-second").forEach(function(item) {
-	$(item).on("click touchstart", function(e) {
+    $(item).on("click touchstart", function(e) {
       var videoSize2 = e.target.dataset.videoSizeButton
-      if (document.querySelectorAll('[data-video-size-button="1"]') && document.querySelectorAll('[data-video-size-button="1"]').length) {
-      	document.querySelectorAll('[data-video-size-button="1"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
-      }
-      if (document.querySelectorAll('[data-video-size-button="2"]') && document.querySelectorAll('[data-video-size-button="2"]').length) {
-      	document.querySelectorAll('[data-video-size-button="2"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
-      }
-      if (document.querySelectorAll('[data-video-size-button="3"]') && document.querySelectorAll('[data-video-size-button="3"]').length) {
-      	document.querySelectorAll('[data-video-size-button="3"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
-      }
-      if (document.querySelectorAll('[data-video-size-button="4"]') && document.querySelectorAll('[data-video-size-button="4"]').length) {
-      	document.querySelectorAll('[data-video-size-button="4"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
-      }
       
-      document.querySelectorAll('[data-video-size-button="' + videoSize2 + '"]').forEach(item => {item.style.fontWeight = 900; item.style.color = "white";})
-			
-	$('[data-video-size="' + selectedVideo2 + '"]').animate({
-		opacity: 0
-      }, 400, function() {
-	      if (selectedVideo2 !== videoSize2) {
-		      $(document.querySelectorAll('[data-video-size="' + videoSize2 + '"]')).css('display', 'block')
-		      $('[data-video-size="' + selectedVideo2 + '"]').css('display', "none")
-		      $(document.querySelectorAll('[data-video-size="' + videoSize2 + '"]')).css('opacity', 0)
-		      $(document.querySelectorAll('[data-video-size="' + videoSize2 + '"]')).animate({
-			opacity: 1
-		      }, 400)
-		      selectedVideo2 = videoSize2
+      if (selectedVideo2 !== videoSize2) {
+	      if (document.querySelectorAll('[data-video-size-button="1"]') && document.querySelectorAll('[data-video-size-button="1"]').length) {
+		document.querySelectorAll('[data-video-size-button="1"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
+	      }
+	      if (document.querySelectorAll('[data-video-size-button="2"]') && document.querySelectorAll('[data-video-size-button="2"]').length) {
+		document.querySelectorAll('[data-video-size-button="2"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
+	      }
+	      if (document.querySelectorAll('[data-video-size-button="3"]') && document.querySelectorAll('[data-video-size-button="3"]').length) {
+		document.querySelectorAll('[data-video-size-button="3"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
+	      }
+	      if (document.querySelectorAll('[data-video-size-button="4"]') && document.querySelectorAll('[data-video-size-button="4"]').length) {
+		document.querySelectorAll('[data-video-size-button="4"]').forEach(item => {item.style.fontWeight = 400; item.style.color = "#979797";})
 	      }
 
-      })
-			
-			
+	      document.querySelectorAll('[data-video-size-button="' + videoSize2 + '"]').forEach(item => {item.style.fontWeight = 900; item.style.color = "white";})
+
+		$('[data-video-size="' + selectedVideo2 + '"]').animate({
+			opacity: 0
+	      }, 400, function() {
+		      if (selectedVideo2 !== videoSize2) {
+			      $(document.querySelectorAll('[data-video-size="' + videoSize2 + '"]')).css('display', 'block')
+			      $('[data-video-size="' + selectedVideo2 + '"]').css('display', "none")
+			      $(document.querySelectorAll('[data-video-size="' + videoSize2 + '"]')).css('opacity', 0)
+			      $(document.querySelectorAll('[data-video-size="' + videoSize2 + '"]')).animate({
+				opacity: 1
+			      }, 400)
+			      selectedVideo2 = videoSize2
+		      }
+
+	      })
+      }
+	    
     })
   })
